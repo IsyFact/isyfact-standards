31d10b32593e963cc1ba0f78090f0cd948b2fceb
Alexander Salvanos <AlexanderSalvanos@msg-systems.com>
2017-09-20 12:01:28 +0200

    Parallel tests.

--- src/test/java/de/bund/bva/isyfact/task/TestOperation2.java
+++ src/test/java/de/bund/bva/isyfact/task/TestOperation2.java
@@ -5,17 +5,13 @@ import de.bund.bva.isyfact.task.model.impl.OperationImpl;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
-public class TestOperation1 extends OperationImpl {
-
-    public TestOperation1() {
-    }
-
+public class TestOperation2 extends OperationImpl {
     @Override
     public void run() {
         try {
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < 10; i++) {
                 TimeUnit.SECONDS.sleep(1);
-                System.out.println(LocalDateTime.now() + " running Operation 1");
+                System.out.println(LocalDateTime.now() + " running Operation 2");
             }
             setHasBeenExecutedSuccessfully(true);
         } catch(Throwable e) {
--- src/test/java/de/bund/bva/isyfact/task/TestTimeoutTaskScheduler.java
+++ src/test/java/de/bund/bva/isyfact/task/TestTimeoutTaskScheduler.java
@@ -18,6 +18,7 @@ import de.bund.bva.isyfact.task.model.Task;
 import de.bund.bva.isyfact.task.model.TaskData;
 import de.bund.bva.isyfact.task.security.SecurityAuthenticator;
 import de.bund.bva.pliscommon.konfiguration.common.Konfiguration;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -46,7 +47,7 @@ public class TestTimeoutTaskScheduler  {
      * Initialize the Tests
      */
     @Before
-    public void initialize() {
+    public void setUp() {
         when(konfiguration.getAsString("isyfact.task.standard.date_time_pattern"))
                 .thenReturn("dd.MM.yyyy HH:mm:ss.SSS");
         when(konfiguration.getAsInteger("isyfact.task.standard.amount_of_threads"))
@@ -70,7 +71,7 @@ public class TestTimeoutTaskScheduler  {
      * @throws Exception
      */
     @Test
-    public void testSchedule() throws Exception {
+    public void testSchedule1() throws Exception {
 
         when(konfiguration.getAsString("isyfact.task.taskTest1.id"))
                 .thenReturn("TaskTest1");
@@ -128,4 +129,79 @@ public class TestTimeoutTaskScheduler  {
         taskScheduler.shutDownNow();
 
     }
+
+
+    /**
+     *
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSchedule2() throws Exception {
+
+        when(konfiguration.getAsString("isyfact.task.taskTest2.id"))
+                .thenReturn("TaskTest2");
+
+        /*
+         * Fake ExecutionDateTime to a delay of 5 seconds, in order to have the test repeatable.
+         */
+        LocalDateTime genericExecutionDateTime = LocalDateTime.now().plusSeconds(5);
+        String sExecutionDateTime = genericExecutionDateTime.format(dateTimeFormatter);
+        System.out.println("sExecutionDateTime:" + sExecutionDateTime);
+        when(konfiguration.getAsString("isyfact.task.taskTest2.executionDateTime"))
+                .thenReturn(sExecutionDateTime);
+
+        when(konfiguration.getAsString("isyfact.task.taskTest2.hostName"))
+                .thenReturn("isyfact-VirtualBox");
+        when(konfiguration.getAsString("isyfact.task.taskTest2.operationName"))
+                .thenReturn("de.bund.bva.isyfact.task.TestOperation2");
+        when(konfiguration.getAsString("isyfact.task.taskTest1.username"))
+                .thenReturn("MyTestUser2");
+        when(konfiguration.getAsString("isyfact.task.taskTest1.password"))
+                .thenReturn("MyTestPasswort2");
+
+        String id = konfiguration.getAsString("isyfact.task.taskTest2.id");
+        String username = konfiguration.getAsString("isyfact.task.taskTest2.username");
+        String password = konfiguration.getAsString("isyfact.task.taskTest2.password");
+        String executionDateTime = konfiguration.getAsString("isyfact.task.taskTest2.executionDateTime");
+        String operationName = konfiguration.getAsString("isyfact.task.taskTest2.operationName");
+        String hostName = konfiguration.getAsString("isyfact.task.taskTest2.hostName");
+
+        TaskDataHandler taskDataHandler = new TaskDataHandlerImpl();
+        TaskData taskData = taskDataHandler.createTaskData(
+                id,
+                username,
+                password,
+                executionDateTime,
+                operationName,
+                hostName
+        );
+
+        TaskHandler taskHandler = new TaskHandlerImpl();
+        Task task = taskHandler.createTask(taskData);
+
+        TaskScheduler taskScheduler = new TaskSchedulerImpl(konfiguration, securityAuthenticator);
+        taskScheduler.schedule(task);
+
+        int delay = task.getExecutionDateTime().getSecond() - LocalDateTime.now().getSecond();
+        System.out.println("Waiting " + delay + " seconds");
+
+        taskScheduler.awaitTerminationInSeconds(20);
+
+        int amount_of_threads = konfiguration.getAsInteger("isyfact.task.standard.amount_of_threads");
+        assertEquals(amount_of_threads, 100);
+
+        System.out.println("ScheduledExecuterService will shut down now!");
+        taskScheduler.shutDownNow();
+
+    }
+
+    /**
+     * Commands after the test
+     */
+    @After
+    public void tearDown() {
+    }
+
+
 }
