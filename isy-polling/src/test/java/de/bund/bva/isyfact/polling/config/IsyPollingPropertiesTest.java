e222b16979a9f9bd0d619af315dcb5cef87c01ee
Björn Saxe <bjoern.saxe@msg.group>
2019-01-17 16:36:07 +0100

    IFS-309: Umstellung Package-Name; Autokonfiguration; Umstellung Unit-Tests

---
+++ isy-polling/src/test/java/de/bund/bva/isyfact/polling/config/IsyPollingPropertiesTest.java
@@ -0,0 +1,75 @@
+package de.bund.bva.isyfact.polling.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class IsyPollingPropertiesTest {
+
+    @Test
+    public void testPropertiesGesetzt() {
+        Map<String, Object> properties = new HashMap<>();
+
+        properties.put("isy.polling.jmx.domain", "testdomain");
+
+
+        properties.put("isy.polling.jmx.verbindungen.server1.host", "host1");
+        properties.put("isy.polling.jmx.verbindungen.server1.port", "9001");
+        properties.put("isy.polling.jmx.verbindungen.server1.benutzer", "userid1");
+        properties.put("isy.polling.jmx.verbindungen.server1.passwort", "pwd1");
+
+        properties.put("isy.polling.jmx.verbindungen.server2.host", "host2");
+        properties.put("isy.polling.jmx.verbindungen.server2.port", "9002");
+        properties.put("isy.polling.jmx.verbindungen.server2.benutzer", "userid2");
+        properties.put("isy.polling.jmx.verbindungen.server2.passwort", "pwd2");
+
+        properties.put("isy.polling.cluster.POSTFACH1_CLUSTER.name", "Postfachabruf-1");
+        properties.put("isy.polling.cluster.POSTFACH1_CLUSTER.wartezeit", "600");
+        properties.put("isy.polling.cluster.POSTFACH2_CLUSTER.name", "Postfachabruf-2");
+        properties.put("isy.polling.cluster.POSTFACH2_CLUSTER.wartezeit", "700");
+        properties.put("isy.polling.cluster.POSTFACH2_CLUSTER.jmxverbindungen", "server1,server2");
+
+        ConfigurableApplicationContext context =
+            new SpringApplicationBuilder().sources(TestConfig.class).properties(properties).run();
+
+        IsyPollingProperties isyPollingProperties = context.getBean(IsyPollingProperties.class);
+
+        assertEquals("testdomain", isyPollingProperties.getJmx().getDomain());
+        assertEquals(2, isyPollingProperties.getJmx().getVerbindungen().size());
+        assertNotNull(isyPollingProperties.getJmx().getVerbindungen().get("server2"));
+        assertEquals("host2", isyPollingProperties.getJmx().getVerbindungen().get("server2").getHost());
+
+        assertEquals("Postfachabruf-1", isyPollingProperties.getCluster().get("POSTFACH1_CLUSTER").getName());
+
+        assertEquals(2,
+            isyPollingProperties.getCluster().get("POSTFACH2_CLUSTER").getJmxverbindungen().size());
+        assertEquals("server1",
+            isyPollingProperties.getCluster().get("POSTFACH2_CLUSTER").getJmxverbindungen().get(0));
+        assertEquals("server2",
+            isyPollingProperties.getCluster().get("POSTFACH2_CLUSTER").getJmxverbindungen().get(1));
+    }
+
+
+    @Test(expected = BeanCreationException.class)
+    public void testPropertiesNichtGesetzt() {
+        Map<String, Object> properties = new HashMap<>();
+
+        properties.put("isy.polling.jmx.verbindungen.server1.port", "-1");
+
+        new SpringApplicationBuilder().sources(TestConfig.class).properties(properties).run();
+    }
+
+    @Configuration
+    @EnableAutoConfiguration
+    static class TestConfig {
+    }
+}
---
+++ isy-polling/src/test/java/de/bund/bva/isyfact/polling/test/TestConfig.java
@@ -0,0 +1,45 @@
+package de.bund.bva.isyfact.polling.test;
+
+import de.bund.bva.isyfact.polling.PollingMBean;
+import de.bund.bva.isyfact.polling.PollingVerwalter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+@EnableAutoConfiguration
+public class TestConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("resources/isy-polling/nachrichten/fehler");
+
+        return messageSource;
+    }
+
+    @Bean
+    public PollingMBean cluster1Monitor(PollingVerwalter pollingVerwalter) {
+        PollingMBean mBean = new PollingMBean();
+        mBean.setClusterId("CLUSTER1");
+        mBean.setPollingVerwalter(pollingVerwalter);
+
+        return mBean;
+    }
+
+    @Bean
+    public PollingMBean cluster2Monitor(PollingVerwalter pollingVerwalter) {
+        PollingMBean mBean = new PollingMBean();
+        mBean.setClusterId("CLUSTER2");
+        mBean.setPollingVerwalter(pollingVerwalter);
+
+        return mBean;
+    }
+
+    @Bean
+    public PollingAktionAusfuehrer pollingAktionAusfuehrer() {
+        return new PollingAktionAusfuehrer();
+    }
+}
