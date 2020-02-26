4cb80ca0643de4b02b1a6446a145d90f6dbddae7
Maike Glowik <maike.glowik@materna.de>
2020-02-26 10:28:01 +0100

    IFS-463 Lese Port für Test aus Properties

--- isy-sst-bridge/isy-serviceapi-sst-bridge/src/test/java/de/bund/bva/pliscommon/bridges/integration/config/TestClientConfiguration.java
+++ isy-sst-bridge/isy-serviceapi-sst-bridge/src/test/java/de/bund/bva/pliscommon/bridges/integration/config/TestClientConfiguration.java
@@ -6,6 +6,7 @@ import de.bund.bva.isyfact.aufrufkontext.impl.AufrufKontextFactoryImpl;
 import de.bund.bva.isyfact.aufrufkontext.stub.AufrufKontextVerwalterStub;
 import de.bund.bva.isyfact.serviceapi.service.httpinvoker.v1_0_0.bridge.util.ServiceApiMapper;
 import de.bund.bva.pliscommon.bridges.integration.sst.PlisTestRemoteBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
@@ -13,23 +14,25 @@ import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
 @Configuration
 public class TestClientConfiguration {
 
-    //aus Isyfact 2.1
+    //aus Isyfact 2.x
     @Bean
     public AufrufKontextVerwalter<AufrufKontext> isyAufrufKontextVerwalter() {
-        AufrufKontextVerwalterStub<AufrufKontext> verwalterStub =  new AufrufKontextVerwalterStub<>();
+        AufrufKontextVerwalterStub<AufrufKontext> verwalterStub = new AufrufKontextVerwalterStub<>();
         verwalterStub.setAufrufKontextFactory(new AufrufKontextFactoryImpl<>());
         return verwalterStub;
     }
 
     //aus der Bridge
     @Bean
-    public ServiceApiMapper serviceApiMapper() { return new ServiceApiMapper(); }
+    public ServiceApiMapper serviceApiMapper() {
+        return new ServiceApiMapper();
+    }
 
     //Definition des Proxy auf Clientseite
     @Bean
-    public HttpInvokerProxyFactoryBean invoker() {
+    public HttpInvokerProxyFactoryBean invoker(@Value("${server.port}") final int port) {
         HttpInvokerProxyFactoryBean invoker = new HttpInvokerProxyFactoryBean();
-        invoker.setServiceUrl("http://localhost:8088/TestService");
+        invoker.setServiceUrl("http://localhost:" + port + "/TestService");
         invoker.setServiceInterface(PlisTestRemoteBean.class);
         return invoker;
     }
