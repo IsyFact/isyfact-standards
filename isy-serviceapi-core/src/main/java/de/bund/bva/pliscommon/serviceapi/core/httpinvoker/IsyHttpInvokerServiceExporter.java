1b5d4866230233f2630aba3d64e8e97fa1693104
Dimitar Popov <dimitar.popov@seitenbau.com>
2020-05-18 12:11:50 +0200

    IFS-564: Javadocs hinzugefügt.

--- isy-serviceapi-core/src/main/java/de/bund/bva/pliscommon/serviceapi/core/httpinvoker/IsyHttpInvokerServiceExporter.java
+++ isy-serviceapi-core/src/main/java/de/bund/bva/pliscommon/serviceapi/core/httpinvoker/IsyHttpInvokerServiceExporter.java
@@ -2,8 +2,14 @@ package de.bund.bva.pliscommon.serviceapi.core.httpinvoker;
 
 import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
 
+/**
+ * {@link HttpInvokerServiceExporter} mit deaktiviertem {@link acceptProxyClasses}.
+ */
 public class IsyHttpInvokerServiceExporter extends HttpInvokerServiceExporter {
 
+    /**
+     * Default Konstruktor.
+     */
     public IsyHttpInvokerServiceExporter() {
         super();
         this.setAcceptProxyClasses(false);
