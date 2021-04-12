58fa4e9e9896f0afe2c3edc8b2e6ecedb3336acd
Dimitar Popov <dimitar.popov@seitenbau.com>
2021-04-12 17:06:52 +0200

    IFS-741: unnötige Variablen aus 01_environment.sql entfernt.

--- isy-persistence/src/main/skripte/db-versioning-templates/db-schema-name/db-update-x.y.z_1/01_environment.sql
+++ isy-persistence/src/main/skripte/db-versioning-templates/db-schema-name/db-update-x.y.z_1/01_environment.sql
@@ -16,54 +16,30 @@
 */
 /*
  Dieses Skript enthält alle Umgebungsvariablen des Systems für die Umgebung.
-*/ 
+*/
 
 -- -------------------------------------
 -- Konfiguration: Datenbankverbindungen
 -- -------------------------------------
--- Oracle-Datenbankverbindung (Connection-String) 
+-- Oracle-Datenbankverbindung (Connection-String)
 -- in der Form <user-id>/<pwd>@<IP-Adresse>:<port>/<db-instanz>
 -- für den SYS-Admin.
 DEFINE SYSADMIN_CONNECTION      = 'template';
 
--- Oracle-Datenbankverbindung (Connection-String) 
+-- Oracle-Datenbankverbindung (Connection-String)
 -- in der Form <user-id>/<pwd>@<IP-Adresse>:<port>/<db-instanz>
 -- für den Benutzer des neu anzulegenden Schemas.
 DEFINE USER_CONNECTION          = 'template';
 
 
 -- -------------------------------------
--- Konfiguration: Tablespace
--- -------------------------------------
--- Name des Tablespace, in dem das Schema angelegt wird.
-DEFINE TABLESPACE_NAME          = 'template';
-
--- Quota des User für das Schema.
-DEFINE TABLESPACE_QUOTA         = 'template';
-
--- Name des Data-Files inklusiv vollständigen Pfad, 
--- das dem Tablespace zugeordnet wird.
-DEFINE DATAFILE_NAME            = 'template';
-
--- Größe des Data-Files incl. Einheit (z.B. 20G für 20 GB).
-DEFINE DATAFILE_SIZE            = 'template';
-
--- Größe des nächsten zu allokierenden Extends 
--- incl. Einheit (z.B. 5M für 5 MB).
-DEFINE AUTOEXTEND_NEXT_SIZE     = 'template'; 
-
--- Maximale Größe des Data-Files incl. Einheit (z.B. 100G für 100GB).
-DEFINE AUTOEXTEND_MAX_SIZE      = 'template';
-
-
--- -------------------------------------
 -- Konfiguration: User
 -- -------------------------------------
--- Name des Users für das Schema (ist gleich dem Schemanamen). 
+-- Name des Users für das Schema (ist gleich dem Schemanamen).
 -- Muss mit <user-id> im Parameter USER_CONNECTION übereinstimmen.
 DEFINE USERNAME                 = 'template';
 
--- Kennwort des Users für das Schema. 
+-- Kennwort des Users für das Schema.
 -- Muss mit <pwd> User im Parameter USER_CONNECTION übereinstimmen.
 DEFINE PASSWORD                 = 'template';
 
