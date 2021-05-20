1186a7f947578b54f7fd6d6c8f1bd416326bbd4b
Timo Bribram <timo.bribram@materna.de>
2021-05-20 11:27:51 +0200

    IFS-875: Lagere Auslesen des Bearer-Tokens in eigene Klasse aus, Teile Tests auf

--- isy-serviceapi-core/src/test/java/de/bund/bva/pliscommon/serviceapi/core/httpinvoker/TimeoutWiederholungHttpInvokerRequestExecutorTest.java
+++ isy-serviceapi-core/src/test/java/de/bund/bva/pliscommon/serviceapi/core/httpinvoker/TimeoutWiederholungHttpInvokerRequestExecutorTest.java
@@ -1,128 +1,55 @@
-/*
- * See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership.
- * The Federal Office of Administration (Bundesverwaltungsamt, BVA)
- * licenses this file to you under the Apache License, Version 2.0 (the
- * License). You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 package de.bund.bva.pliscommon.serviceapi.core.httpinvoker;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.net.HttpURLConnection;
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.http.HttpHeaders;
-import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
-import org.springframework.remoting.httpinvoker.SimpleHttpInvokerRequestExecutor;
 
-import de.bund.bva.pliscommon.aufrufkontext.AufrufKontext;
+import de.bund.bva.pliscommon.aufrufkontext.AufrufKontextVerwalter;
 import de.bund.bva.pliscommon.aufrufkontext.stub.AufrufKontextVerwalterStub;
 import de.bund.bva.pliscommon.serviceapi.core.httpinvoker.stub.HttpUrlConnectionStub;
 import de.bund.bva.pliscommon.serviceapi.core.httpinvoker.stub.TimeoutWiederholungHttpInvokerRequestExecutorStub;
-import de.bund.bva.pliscommon.serviceapi.service.httpinvoker.v1_0_0.DummyServiceImpl;
-import de.bund.bva.pliscommon.serviceapi.service.httpinvoker.v1_0_0.DummyServiceRemoteBean;
 
-import junit.framework.TestCase;
+/**
+ * Plain unit tests for {@link TimeoutWiederholungHttpInvokerRequestExecutor}.
+ */
+public class TimeoutWiederholungHttpInvokerRequestExecutorTest {
 
-public class TimeoutWiederholungHttpInvokerRequestExecutorTest extends TestCase {
+    /** Some value for the content length, doesn't get actually used. */
+    private static final int CONTENT_LENGTH = 1;
 
-    private DummyServer dummyServer;
-    private DummyServiceImpl dummyService;
-    private String serviceUrl;
+    /** AufrufKontextVerwalter to set bearer token in. */
+    private AufrufKontextVerwalter<?> aufrufKontextVerwalterStub;
 
-    @Override
-    protected void setUp() throws Exception {
-        dummyServer = new DummyServer();
-        serviceUrl = dummyServer.getServiceUrl();
-        dummyService = dummyServer.getDummyService();
-    }
+    /** Stubbed request executor. */
+    private TimeoutWiederholungHttpInvokerRequestExecutor executorStub;
 
-    @Override
-    protected void tearDown() throws Exception {
-        Thread.sleep(1000);
-        dummyServer.stop();
-    }
+    /** Stubbed connection that doesn't connect to anything. */
+    private HttpURLConnection connection;
 
-    public DummyServiceRemoteBean createHttpInvokerProxy(SimpleHttpInvokerRequestExecutor simpleHttpInvokerRequestExecutor) {
-        HttpInvokerProxyFactoryBean proxyFactory = new HttpInvokerProxyFactoryBean();
-        proxyFactory.setHttpInvokerRequestExecutor(simpleHttpInvokerRequestExecutor);
-        proxyFactory.setServiceUrl(serviceUrl);
-        proxyFactory.setServiceInterface(DummyServiceRemoteBean.class);
-        proxyFactory.afterPropertiesSet();
-        return (DummyServiceRemoteBean) proxyFactory.getObject();
-    }
-
-    public void testTimeoutKurz() {
-        dummyService.setWaitTime(0);
-        TimeoutWiederholungHttpInvokerRequestExecutor timeoutExecutor =
-                new TimeoutWiederholungHttpInvokerRequestExecutor(new AufrufKontextVerwalterStub<>());
-        timeoutExecutor.setTimeout(100);
-
-        DummyServiceRemoteBean dummyServiceRemoteBean = createHttpInvokerProxy(timeoutExecutor);
-        assertEquals("Hello", dummyServiceRemoteBean.ping("Hello"));
-        assertEquals(1, dummyService.getAnzahlAufrufe());
-    }
-
-    public void testTimeoutLang() {
-        dummyService.setWaitTime(30000);
-        TimeoutWiederholungHttpInvokerRequestExecutor timeoutExecutor =
-                new TimeoutWiederholungHttpInvokerRequestExecutor(new AufrufKontextVerwalterStub<>());
-        timeoutExecutor.setTimeout(60000);
-
-        DummyServiceRemoteBean dummyServiceRemoteBean = createHttpInvokerProxy(timeoutExecutor);
-        assertEquals("Hello", dummyServiceRemoteBean.ping("Hello"));
-        assertEquals(1, dummyService.getAnzahlAufrufe());
-    }
-
-    public void testAufrufWiederholung() {
-        dummyService.setWaitTime(200);
-        TimeoutWiederholungHttpInvokerRequestExecutor timeoutExecutor =
-                new TimeoutWiederholungHttpInvokerRequestExecutor(new AufrufKontextVerwalterStub<>());
-        timeoutExecutor.setTimeout(50);
-        timeoutExecutor.setWiederholungenAbstand(500);
-        timeoutExecutor.setAnzahlWiederholungen(10);
-
-        long t0 = System.currentTimeMillis();
-        try {
-            DummyServiceRemoteBean dummyServiceRemoteBean = createHttpInvokerProxy(timeoutExecutor);
-            dummyServiceRemoteBean.ping("Hello");
-            fail("Exception erwartet");
-        } catch (Throwable t) {
-            long t1 = System.currentTimeMillis();
-            assertTrue(InterruptedIOException.class.isAssignableFrom(t.getCause().getClass()));
-            assertEquals(10, dummyService.getAnzahlAufrufe());
-            assertTrue("Wiederholungspausen wurde nicht eingehalten: " + (t1 - t0), (t1 - t0) > 4000);
-        }
+    @Before
+    public void setUp() {
+        aufrufKontextVerwalterStub = new AufrufKontextVerwalterStub<>();
+        aufrufKontextVerwalterStub.setBearerToken(null);
+        executorStub = new TimeoutWiederholungHttpInvokerRequestExecutorStub(aufrufKontextVerwalterStub);
+        connection = new HttpUrlConnectionStub(null);
     }
 
     /**
      * Test if the bearer token gets set in the Authorization header.
      */
+    @Test
     public void testPrepareConnectionWithToken() {
-        final int timeout = 50;
         final String token = "testToken1234";
 
-        AufrufKontextVerwalterStub<AufrufKontext> aufrufKontextVerwalterStub = new AufrufKontextVerwalterStub<>();
         aufrufKontextVerwalterStub.setBearerToken(token);
-
-        TimeoutWiederholungHttpInvokerRequestExecutorStub executorStub =
-                new TimeoutWiederholungHttpInvokerRequestExecutorStub(aufrufKontextVerwalterStub);
-        executorStub.setTimeout(timeout);
-
-        HttpURLConnection connection = new HttpUrlConnectionStub(null);
-
         try {
-            executorStub.prepareConnection(connection, 1);
+            executorStub.prepareConnection(connection, CONTENT_LENGTH);
         } catch (IOException e) {
             fail("Expected no exception.");
         }
@@ -130,32 +57,35 @@ public class TimeoutWiederholungHttpInvokerRequestExecutorTest extends TestCase 
         List<String> authHeader = connection.getRequestProperties().get(HttpHeaders.AUTHORIZATION);
         assertEquals(1, authHeader.size());
         assertEquals("Bearer " + token, authHeader.get(0));
-        assertEquals(timeout, connection.getConnectTimeout());
-        assertEquals(timeout, connection.getReadTimeout());
     }
 
     /**
      * Test if the Authorization header is not set if the bearer token is {@code null}.
      */
+    @Test
     public void testPrepareConnectionWithoutToken() {
-        final int timeout = 50;
-
-        AufrufKontextVerwalterStub<AufrufKontext> aufrufKontextVerwalterStub = new AufrufKontextVerwalterStub<>();
-        aufrufKontextVerwalterStub.setBearerToken(null);
-
-        TimeoutWiederholungHttpInvokerRequestExecutorStub executorStub =
-                new TimeoutWiederholungHttpInvokerRequestExecutorStub(aufrufKontextVerwalterStub);
-        executorStub.setTimeout(timeout);
-
-        HttpURLConnection connection = new HttpUrlConnectionStub(null);
-
         try {
-            executorStub.prepareConnection(connection, 1);
+            executorStub.prepareConnection(connection, CONTENT_LENGTH);
         } catch (IOException e) {
             fail("Expected no exception.");
         }
 
         assertNull(connection.getRequestProperties().get(HttpHeaders.AUTHORIZATION));
+    }
+
+    /**
+     * Test if the read and connect timeouts get properly set.
+     */
+    @Test
+    public void testTimeoutsGetSet() {
+        final int timeout = 50;
+
+        executorStub.setTimeout(timeout);
+        try {
+            executorStub.prepareConnection(connection, CONTENT_LENGTH);
+        } catch (IOException e) {
+            fail("Expected no exception.");
+        }
         assertEquals(timeout, connection.getConnectTimeout());
         assertEquals(timeout, connection.getReadTimeout());
     }
