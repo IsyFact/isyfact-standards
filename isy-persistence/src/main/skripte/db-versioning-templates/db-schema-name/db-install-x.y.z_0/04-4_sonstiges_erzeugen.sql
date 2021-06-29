96e3b98d3df3b02fc6ade87e5c58931f42843cc0
Daniel Kurowski <daniel.kurowski@materna.de>
2021-06-29 10:43:15 +0200

    IFS-1066: Übersetzung

--- isy-persistence/src/main/skripte/db-versioning-templates/db-schema-name/db-install-x.y.z_0/04-4_sonstiges_erzeugen.sql
+++ isy-persistence/src/main/skripte/db-versioning-templates/db-schema-name/db-install-x.y.z_0/04-4_sonstiges_erzeugen.sql
@@ -1,23 +1,9 @@
-/*
- See the NOTICE file distributed with this work for additional
- information regarding copyright ownership.
- The Federal Office of Administration (Bundesverwaltungsamt, BVA)
- licenses this file to you under the Apache License, Version 2.0 (the
- License). You may not use this file except in compliance with the
- License. You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- implied. See the License for the specific language governing
- permissions and limitations under the License.
-*/
-/*
- Weitere system-spezifische Objekte von diesem Skript erzeugt werden.
-*/
+-- Dieses Skript erzeugt weitere system-spezifische Objekte.
+-- ----------------------------------------------------------------------------------------------------------------
+-- This script creates additional system-specific objects
 
 -- ----------------------------------------------------------------------------------------------------------------
--- Beispiel: Erstellen einer Queue-Tabelle und einer Queue.
+-- Erstellen einer Queue-Tabelle und einer Queue.
+-- ----------------------------------------------------------------------------------------------------------------
+-- Create a queue table and a queue.
 -- ----------------------------------------------------------------------------------------------------------------
--- isy-persistence/src/main/skripte/db-versioning-templates/uebergreifend/db-install-x.y.z_0/00_install-main.sql
+++ isy-persistence/src/main/skripte/db-versioning-templates/uebergreifend/db-install-x.y.z_0/00_install-main.sql
@@ -1,24 +1,9 @@
-/*
- See the NOTICE file distributed with this work for additional
- information regarding copyright ownership.
- The Federal Office of Administration (Bundesverwaltungsamt, BVA)
- licenses this file to you under the Apache License, Version 2.0 (the
- License). You may not use this file except in compliance with the
- License. You may obtain a copy of the License at
+-- Dieses Skript führt die übergreifenden Operationen schrittweise durch.
+-- Zum Loggen der Schritte wird das Hauptschema verwendet.
+-- ----------------------------------------------------------------------------------------------------------------
+-- This script executes the crosscutting operations step by step.
+-- The main schema is used to log the steps.
 
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- implied. See the License for the specific language governing
- permissions and limitations under the License.
-*/
-
-/*
- Dieses Skript fuehrt die übergreifenden Operationen schrittweise durch. 
- Zum Loggen der Schritte wird das Hauptschema verwendet.
-*/
 
 WHENEVER OSERROR EXIT 9
 WHENEVER SQLERROR EXIT FAILURE SQL.SQLCODE ROLLBACK
@@ -35,17 +20,21 @@ DEFINE SCHEMA_UPDATE = '0';
 
 
 -- Schritt 1: Umgebungsvariablen laden
+-- ----------------------------------------------------------------------------------------------------------------
+-- Step 1: Load environment variables
 SPOOL &P_LOG_DATEI
 @&P_ENVIRONMENT_SCRIPT
 
 -- Pruefen, dass der Status des Hauptschemas auf "bereit" steht
+-- ----------------------------------------------------------------------------------------------------------------
+-- Check if the status of the main schema is 'bereit' (ready)
 CONNECT &SYSADMIN_CONNECTION
 declare
-statushauptschema varchar2(6);
+  statushauptschema varchar2(6);
 begin
-SELECT STATUS into statushauptschema
-from &SCHEMA_NAME_HAUPTSCHEMA..m_schema_version
-where version_nummer = '&SCHEMA_VERSION' and update_nummer = '&SCHEMA_UPDATE' and status = 'bereit';
+  SELECT STATUS into statushauptschema
+  from &SCHEMA_NAME_HAUPTSCHEMA..m_schema_version
+  where version_nummer = '&SCHEMA_VERSION' and update_nummer = '&SCHEMA_UPDATE' and status = 'bereit';
 exception
   when no_data_found
   then raise_application_error(-20000, 'Die übergreifenden Operationen konnten nicht ausgeführt werden, weil das Hauptschema nicht auf bereit steht.');
@@ -54,27 +43,39 @@ end;
 DISCONNECT
 
 -- Schritt 2: Schemaübergreifende Rechte setzen
+-- ----------------------------------------------------------------------------------------------------------------
+-- Step 2: Set cross-schema rights
 CONNECT &SYSADMIN_CONNECTION
 @92_user.sql
 DISCONNECT
 
 -- Schritt 3: Schemaübergreifende Operationen aufrufen
+-- ----------------------------------------------------------------------------------------------------------------
+-- Step 3: Execute cross-schema operations
 CONNECT &SYSADMIN_CONNECTION
 @99_starte-skript-mit-logging.sql 93-1_uebergreifende_operation_xxx.sql '93-1' 'Schemaübergreifende Operation xxx'
 -- Hier können weitere Skripte folgen
+-- ----------------------------------------------------------------------------------------------------------------
+-- Additional scripts may follow here
 DISCONNECT
 
 -- Schritt 4: Abschlussbearbeitung
+-- ----------------------------------------------------------------------------------------------------------------
+-- Step 4: Perform closing operations
 CONNECT &SYSADMIN_CONNECTION
 @99_starte-skript-mit-logging.sql 94_abschlussbearbeitung.sql '94' 'Abschlussbearbeitung durchfuehren'
 COMMIT;
 DISCONNECT
 
 -- Schritt 5: Benutzerrechte entziehen
+-- ----------------------------------------------------------------------------------------------------------------
+-- Step 5: Remove user privileges
 CONNECT &SYSADMIN_CONNECTION
 @99_starte-skript-mit-logging.sql 95_user_rechte_entziehen.sql '95' 'Benutzerrechte entziehen'
 
 -- Das Hauptschema auf 'gueltig' setzen.
+-- ----------------------------------------------------------------------------------------------------------------
+-- Set the main schema to 'gueltig' (valid).
 update &SCHEMA_NAME_HAUPTSCHEMA..M_SCHEMA_VERSION set STATUS = 'gueltig' where VERSION_NUMMER = '&SCHEMA_VERSION' and UPDATE_NUMMER = '&SCHEMA_UPDATE';
 COMMIT;
 DISCONNECT
