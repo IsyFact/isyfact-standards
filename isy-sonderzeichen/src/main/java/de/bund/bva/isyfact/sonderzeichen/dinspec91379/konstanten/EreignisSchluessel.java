ea23d1ee63fdacc041826047596ed4e88255a9da
Timo Bribram <timo.bribram@materna.de>
2021-07-09 15:48:27 +0200

    feat(Sonderzeichen): IFS-1035 Add function for validating strings against DIN SPEC data types
    * Add new class ZeichenValidator
      * This provides a method for
        checking a given string against a DIN SPEC 91379 data type
    * Add tests for the new class
    * Add category (.kat) files for each character group defined by DIN SPEC 91379

--- isy-sonderzeichen/src/main/java/de/bund/bva/isyfact/sonderzeichen/dinspec91379/konstanten/EreignisSchluessel.java
+++ isy-sonderzeichen/src/main/java/de/bund/bva/isyfact/sonderzeichen/dinspec91379/konstanten/EreignisSchluessel.java
@@ -21,6 +21,10 @@ public final class EreignisSchluessel {
     /** Event key for transformation. */
     public static final String TRANSFORMATION = "EPLSOND00001";
 
+    /** Event key for validation. */
+    public static final String VALIDIERUNG = "EPLSOND00002";
+
     private EreignisSchluessel() {
     }
+
 }
