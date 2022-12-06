0b5a643faf1c560440e74906fc8f6f1bbab744ff
Friedrich Greiner (SEITENBAU) <friedrich.greiner@seitenbau.com>
2022-12-06 10:08:08 +0100

    test: IFS-1820 disable h2 oracle mode during tests

--- isy-persistence/src/test/resources/sql.persistence/create_version_table.sql
+++ isy-persistence/src/test/resources/sql.persistence/create_version_table.sql
@@ -5,8 +5,8 @@
 -- Verwaltungstabellen anlegen
 create table m_schema_version
 (
-    version_nummer varchar2(25 char),
-    update_nummer  varchar2(5 char),
-    status         varchar2(25 char),
+    version_nummer varchar2(25),
+    update_nummer  varchar2(5),
+    status         varchar2(25),
     constraint pk_m_schema_version primary key (version_nummer)
 );
