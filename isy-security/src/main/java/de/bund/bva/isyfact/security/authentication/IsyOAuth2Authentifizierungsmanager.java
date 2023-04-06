0a595cd7daa270a0ace9bd18412a8e67b4ce161d
Friedrich Greiner (SEITENBAU) <friedrich.greiner@seitenbau.com>
2023-04-06 13:18:46 +0200

    feat: IFS-2400 add implementation of Authentifizierungsmanager (IsyOAuth2Authentifizierungsmanager)

---
+++ isy-security/src/main/java/de/bund/bva/isyfact/security/authentication/IsyOAuth2Authentifizierungsmanager.java
@@ -0,0 +1,17 @@
+package de.bund.bva.isyfact.security.authentication;
+
+import de.bund.bva.isyfact.security.Authentifizierungsmanager;
+
+
+public class IsyOAuth2Authentifizierungsmanager implements Authentifizierungsmanager {
+
+    @Override
+    public void authentifiziereClient(String registrationId) {
+
+    }
+
+    @Override
+    public void authentifiziereSystem(String registrationId) {
+
+    }
+}
