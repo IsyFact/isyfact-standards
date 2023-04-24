4792f4e7c1fa0f39311d7f5c8d99862c48ce359e
Friedrich Greiner (SEITENBAU) <friedrich.greiner@seitenbau.com>
2023-04-24 16:53:23 +0200

    docs: IFS-2400 describe usage of the Security and Authentifizierungsmanager interface

--- isy-security/src/main/java/de/bund/bva/isyfact/security/Security.java
+++ isy-security/src/main/java/de/bund/bva/isyfact/security/Security.java
@@ -10,17 +10,21 @@ public interface Security {
 
     /**
      * Get a list of all roles stored in the system.
+     *
      * @return All roles stored in the RollenRechte-mapping.
      */
     Set<String> getAlleRollen();
 
     /**
+     * The {@link Authentifizierungsmanager} allows manual authentication of clients.
      *
-     * @return {@link Authentifizierungsmanager}
+     * @return Optional of {@link Authentifizierungsmanager} or {@code null if the system
+     * acts as ressource-server and the {@link Authentifizierungsmanager} is not configured
      */
     Optional<Authentifizierungsmanager> getAuthentifizierungsManager();
 
     /**
+     * The {@link Berechtigungsmanager} allows checking privileges in the currently authenticated security context.
      *
      * @return {@link Berechtigungsmanager}
      */
