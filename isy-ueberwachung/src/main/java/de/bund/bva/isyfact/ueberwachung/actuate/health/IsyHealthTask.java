1de8dff4487aa0283643b2bf4c2c70e60d87154d
Friedrich Greiner (SEITENBAU) <friedrich.greiner@seitenbau.com>
2023-05-05 15:51:11 +0200

    fix: IFS-2153: remove redundant Annotation

--- isy-ueberwachung/src/main/java/de/bund/bva/isyfact/ueberwachung/actuate/health/IsyHealthTask.java
+++ isy-ueberwachung/src/main/java/de/bund/bva/isyfact/ueberwachung/actuate/health/IsyHealthTask.java
@@ -3,7 +3,6 @@ package de.bund.bva.isyfact.ueberwachung.actuate.health;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import de.bund.bva.isyfact.logging.IsyLogger;
@@ -12,7 +11,6 @@ import de.bund.bva.isyfact.logging.IsyLoggerFactory;
 /**
  * This Task updates the cache in {@link IsyHealthContributorRegistryCache}.
  */
-@Component
 public class IsyHealthTask {
 
     private static final IsyLogger LOGISY = IsyLoggerFactory.getLogger(IsyHealthTask.class);
