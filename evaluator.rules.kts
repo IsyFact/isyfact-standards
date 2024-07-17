761e0ab9073208d62a25b4f1a2676eb276d9b61f
Marius Arlt <marius.arlt@msg.group>
2024-07-17 11:41:43 +0200

    ci: ISY-3767: removed deprecated white list

--- evaluator.rules.kts
+++ evaluator.rules.kts
@@ -1,13 +1,5 @@
 val allowedLicenses = licenseClassifications.licensesByCategory["allow"].orEmpty()
 
-//fun PackageRule.LicenseRule.isHandled() =
-//        object : RuleMatcher {
-//            override val description = "isHandled($license)"
-//
-//            override fun matches() =
-//                    license in allowedLicenses && ("-exception" !in license.toString() || " WITH " in license.toString())
-//        }
-
 rule("LicenseWhitelist") {
     licenseFindings.forEach { finding ->
         val license = finding.license
