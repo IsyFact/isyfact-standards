d5d35fefceb921c35195f8a09e6b67b7fd1b3fd0
Marius Arlt <marius.arlt@msg.group>
2024-07-17 11:54:30 +0200

    ci: ISY-3767: deleted license management since these files are defined in template repository

--- evaluator.rules.kts
+++
@@ -1,7 +0,0 @@
-funPackageRule.LicenseRule.isHandled()=
-        object:RuleMatcher{
-            overridevaldescription="isHandled($license)"
-
-            overridefunmatches()=
-            licenseinallowedLicenses&&("-exception"!inlicense.toString()||"WITH"inlicense.toString())
-        }
