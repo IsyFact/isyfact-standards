24883de740f16da7fd374cf267c9702941aa9d03
Friedrich Greiner <friedrich.greiner@seitenbau.com>
2024-09-10 09:12:11 +0200

    test(persistence): IFS-4010 add AutoConfiguration Tests
    * test valid AutoConfiguration
    * expect circular reference

--- isy-persistence/src/test/java/de/bund/bva/isyfact/persistence/autoconfigure/IsyPersistenceAutoConfigurationTest.java
+++ isy-persistence/src/test/java/de/bund/bva/isyfact/persistence/autoconfigure/IsyPersistenceAutoConfigurationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanCurrentlyInCreationException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
@@ -47,6 +48,62 @@ class IsyPersistenceAutoConfigurationTest {
                 .cause()
                 .isInstanceOf(UnsatisfiedDependencyException.class);
         });
+    }
+
+    @Test
+    void expectCyclicReferenceException() {
+        contextRunner.withUserConfiguration(TestConfig.class)
+            .withPropertyValues(
+                "isy.logging.anwendung.name=test",
+                "isy.logging.anwendung.typ=test",
+                "isy.logging.anwendung.version=test",
+                // h2 profile properties
+                "isy.persistence.datasource.url=jdbc:h2:mem:isypersistence;TRACE_LEVEL_FILE=4;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false",
+                "isy.persistence.datasource.username=isypersistence",
+                "isy.persistence.datasource.password=password",
+                "isy.persistence.datasource.driver-class-name=org.h2.Driver",
+                "isy.persistence.datasource.type=org.springframework.jdbc.datasource.SimpleDriverDataSource",
+                "isy.persistence.datasource.schema-version=4.1.0",
+                "isy.persistence.datasource.schema-invalid-version-action=fail",
+                "spring.jpa.properties.hibernate.hbm2ddl.auto=none"
+            )
+            .run(context -> {
+                assertThat(context).hasFailed()
+                    .getFailure()
+                    .cause()
+                    .isInstanceOf(BeanCurrentlyInCreationException.class)
+                    .hasMessageContaining("Is there an unresolvable circular reference?");
+            });
+    }
+
+    @Test
+    void validAutoConfiguration() {
+        contextRunner.withUserConfiguration(
+                TestConfig.class,
+                DataSourceInitializerTest.TestDataSourceInitializationConfig.class
+            )
+            .withPropertyValues(
+                "isy.logging.anwendung.name=test",
+                "isy.logging.anwendung.typ=test",
+                "isy.logging.anwendung.version=test",
+                // fix cyclic reference
+                "spring.sql.init.mode=never",
+                // h2 profile properties
+                "isy.persistence.datasource.url=jdbc:h2:mem:isypersistence;TRACE_LEVEL_FILE=4;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false",
+                "isy.persistence.datasource.username=isypersistence",
+                "isy.persistence.datasource.password=password",
+                "isy.persistence.datasource.driver-class-name=org.h2.Driver",
+                "isy.persistence.datasource.type=org.springframework.jdbc.datasource.SimpleDriverDataSource",
+                "isy.persistence.datasource.schema-version=4.1.0",
+                "isy.persistence.datasource.schema-invalid-version-action=fail",
+                "spring.jpa.properties.hibernate.hbm2ddl.auto=none"
+            ).run(context -> {
+                assertThat(context)
+                    .hasNotFailed()
+                    .hasSingleBean(IsyDataSource.class)
+                    .hasBean("dataSource")
+                    .hasBean("appDataSource");
+            });
     }
 
     @Configuration
