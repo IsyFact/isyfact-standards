21ff67d7b1607adb470e906147e2f2e0cc2fa33b
Valeska Marz <valeska.marz@materna.group>
2024-10-29 12:05:08 +0100

    feat(isy-task): IFS-4329 add @Profile("!batch") to IsyTaskAutoConfiguration

--- isy-task/src/main/java/de/bund/bva/isyfact/task/autoconfigure/IsyTaskAutoConfiguration.java
+++ isy-task/src/main/java/de/bund/bva/isyfact/task/autoconfigure/IsyTaskAutoConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -23,6 +24,7 @@ import io.micrometer.core.aop.TimedAspect;
 @Configuration
 @EnableConfigurationProperties
 @EnableScheduling
+@Profile("!batch")
 public class IsyTaskAutoConfiguration {
 
     @Bean
