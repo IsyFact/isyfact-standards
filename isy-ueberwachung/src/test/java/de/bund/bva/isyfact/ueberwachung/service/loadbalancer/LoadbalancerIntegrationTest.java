e5e16635a9eac22e8752c95f6011de6b44e15ea5
hgruber-anton <helmut-anton.gruber@materna.group>
2025-12-16 13:57:44 +0100

    feat: IFS-4911 remove unused imports

--- isy-ueberwachung/src/test/java/de/bund/bva/isyfact/ueberwachung/service/loadbalancer/LoadbalancerIntegrationTest.java
+++ isy-ueberwachung/src/test/java/de/bund/bva/isyfact/ueberwachung/service/loadbalancer/LoadbalancerIntegrationTest.java
@@ -1,33 +1,14 @@
 package de.bund.bva.isyfact.ueberwachung.service.loadbalancer;
 
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.core.Appender;
-import de.bund.bva.isyfact.ueberwachung.actuate.health.nachbarsystemcheck.model.NachbarsystemHealth;
 import de.bund.bva.isyfact.ueberwachung.autoconfigure.IsyHealthAutoConfiguration;
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
-import org.springframework.boot.actuate.health.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -47,7 +28,7 @@ public class LoadbalancerIntegrationTest {
 
     @Test
     //Test returns forbidden because isAlive file is not found in temp test tomcat
-    public void testDoGetIsAlive() throws ServletException, IOException {
+    public void testDoGetIsAlive() {
         urlCall("/Loadbalancer").expectStatus().isForbidden();
 
     }
