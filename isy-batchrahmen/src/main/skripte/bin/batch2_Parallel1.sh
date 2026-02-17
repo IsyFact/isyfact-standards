8f14cc2437d6cae3d8205087e0a5fed1c79a0e9e
Fabian Perl <fabian.perl@seitenbau.com>
2026-02-17 08:00:49 +0100

    build: IFS-5214 removes isy-batchrahmen maven module

--- isy-batchrahmen/src/main/skripte/bin/batch2_Parallel1.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-BATCHDIR=`dirname $0`
-$BATCHDIR/batch-ausfuehren.sh -start -cfg /resources/batch/batch-2-config.properties -Batchrahmen.Ergebnisdatei /batch-2_1_out.xml BatchId=Batch2_1
