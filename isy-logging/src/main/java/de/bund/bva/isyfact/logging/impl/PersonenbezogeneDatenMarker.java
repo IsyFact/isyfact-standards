bb379e8709ff276d51ce54009c5a5fe3b6964239
Niklas Schulpius <niklas.schulpius@seitenbau.com>
2026-03-26 16:20:18 +0100

    fix: IFS-5204 add missing serialVersionUID

--- isy-logging/src/main/java/de/bund/bva/isyfact/logging/impl/PersonenbezogeneDatenMarker.java
+++ isy-logging/src/main/java/de/bund/bva/isyfact/logging/impl/PersonenbezogeneDatenMarker.java
@@ -1,6 +1,11 @@
 package de.bund.bva.isyfact.logging.impl;
 
+import java.io.Serial;
+
 public class PersonenbezogeneDatenMarker extends AbstractIsyDatentypMarker {
+
+    @Serial
+    private static final long serialVersionUID = 7491072361151423674L;
 
     /**
      * Ein Marker für personenbezogene Daten.
