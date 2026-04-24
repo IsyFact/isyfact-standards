85d26ec82f9fc49c7d60b72254f6227f2a8542be
Henning Plump <henning.plump@materna.group>
2026-04-24 12:09:53 +0200

    docs: IFS-5434 update documentation

--- isyfact-standards-doc/src/docs/antora/modules/isy-ueberwachung/partials/IsyActuatorSecurityAutoConfiguration.java
+++ isyfact-standards-doc/src/docs/antora/modules/isy-ueberwachung/partials/IsyActuatorSecurityAutoConfiguration.java
@@ -1,5 +1,11 @@
 package de.bund.bva.isyfact.ueberwachung.autoconfigure;
 
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
@@ -7,55 +13,70 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /* tag::actuatorSecurity[] */
 
 /**
- * Configures HttpBasic authentication for actuator endpoints.
- * actuator/health needs no authentification
+ * Configures OAuth2 authentication for actuator endpoints.
+ * actuator/health needs no authentication
  */
 @AutoConfiguration
 @EnableWebSecurity
 @ConditionalOnClass({SecurityFilterChain.class, HttpSecurity.class})
 public class IsyActuatorSecurityAutoConfiguration {
 
-    /** Endpoint role to identify the actuator Endpoint Admin. */
-    public static final String ENDPOINT_ROLE = "ENDPOINT_ADMIN";
-    public static final String URL = "jwk-set-uri";
-
+    /**
+     * OpenID connect certificate‑endpoint to validate token against.
+     */
+    @Value("${isy.ueberwachung.security.jwk-set-uri:}")
+    private String jwkSetUri;
 
     @Bean
     @Order(10)
     @ConditionalOnMissingBean(name = "actuatorSecurityFilterChain")
     public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .securityMatcher(EndpointRequest.toAnyEndpoint())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-                        .anyRequest().hasRole(ENDPOINT_ROLE));
+                        .anyRequest().authenticated())
+                .sessionManagement(sessionConfig ->
+                        sessionConfig
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.sessionManagement(sessionConfig ->
-                sessionConfig
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.oauth2ResourceServer((rs) ->
-                rs.jwt((jwt) -> jwt.decoder(jwtDecoder()))
-        );
+        if (jwkSetUri != null && !jwkSetUri.isEmpty()) {
+            // secure actuator endpoints with JWT token, except health
+            http.oauth2ResourceServer(oauth -> oauth
+                    .jwt(jwt -> jwt
+                            .jwkSetUri(jwkSetUri)
+                    )
+            );
+        } else {
+            http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        }
 
         return http.build();
     }
 
+    /**
+     * Delivers 401 every request, if no decoder is defined.
+     */
     @Bean
     @ConditionalOnMissingBean(name = "jwtDecoder")
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(URL).build();
+        String secret = UUID.randomUUID().toString();
+        return NimbusJwtDecoder.withSecretKey(
+                new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256")
+        ).build();
     }
+
 }
 /* end::actuatorSecurity[] */
